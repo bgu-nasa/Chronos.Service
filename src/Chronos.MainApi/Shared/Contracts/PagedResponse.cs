61207af8ed83cec29f3addb2fc4406df3dc25983
Adam Rammal <adamrammal4@gmail.com>
2026-04-03 01:08:44 +0300

    adding paged response so ican use it in all controlles

---
+++ src/Chronos.MainApi/Shared/Contracts/PagedResponse.cs
@@ -0,0 +1,9 @@
+namespace Chronos.MainApi.Shared.Contracts;
+
+public class PagedResponse<T>
+{
+    public List<T> Items { get; set; } = new();
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+}
