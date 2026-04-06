663bb5baf5397e1cc1390a9c43697193ef84e4fc
adamrammal <128306372+adamrammal@users.noreply.github.com>
2026-04-06 17:00:14 +0300

    Bugfix/adamrammal/47/get activties by schedulig period (#174)
    * implmented logic of get activity by scheduling period
    
    * impl controller
    
    * adding logger

--- src/Chronos.MainApi/Resources/Controllers/SubjectController.cs
+++ src/Chronos.MainApi/Resources/Controllers/SubjectController.cs
@@ -138,10 +138,23 @@ public class SubjectController(
     {
         logger.LogInformation("Get activities endpoint was called.");
         var organizationId = ControllerUtils.GetOrganizationIdAndFailIfMissing(HttpContext, logger);
-        
+
         var activities = await activityService.GetActivitiesAsync(organizationId);
         var activityResponses = activities.Select(a => a.ToActivityResponse()).ToList();
-        
+
+        return Ok(activityResponses);
+    }
+
+    [Authorize(Policy = "OrgRole:Viewer")]
+    [HttpGet("activities/scheduling-period/{schedulingPeriodId}")]
+    public async Task<IActionResult> GetActivitiesBySchedulingPeriodAsync(Guid schedulingPeriodId)
+    {
+        logger.LogInformation("Get activities by scheduling period endpoint was called for scheduling period {SchedulingPeriodId}", schedulingPeriodId);
+        var organizationId = ControllerUtils.GetOrganizationIdAndFailIfMissing(HttpContext, logger);
+
+        var activities = await activityService.GetActivitiesBySchedulingPeriodAsync(organizationId, schedulingPeriodId);
+        var activityResponses = activities.Select(a => a.ToActivityResponse()).ToList();
+
         return Ok(activityResponses);
     }
     
--- src/Chronos.MainApi/Resources/Services/IActivityService.cs
+++ src/Chronos.MainApi/Resources/Services/IActivityService.cs
@@ -8,6 +8,7 @@ public interface IActivityService
     Task<Activity> GetActivityAsync(Guid organizationId, Guid activityId);
     Task<List<Activity>> GetActivitiesAsync(Guid organizationId);
     Task<List<Activity>> GetActivitiesBySubjectAsync(Guid organizationId, Guid subjectId);
+    Task<List<Activity>> GetActivitiesBySchedulingPeriodAsync(Guid organizationId, Guid schedulingPeriodId);
     Task UpdateActivityAsync(Guid organizationId, Guid activityId, Guid subjectId, Guid assignedUserId, string activityType, int? expectedStudents, int duration);
     Task DeleteActivityAsync(Guid organizationId, Guid activityId);
 }
