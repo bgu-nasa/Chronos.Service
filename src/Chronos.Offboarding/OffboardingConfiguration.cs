272813a424b6db36e8308387a90e5bb30c308eb7
ShalevKk <kayatsha@post.bgu.ac.il>
2026-04-12 23:34:15 +0300

    Defined CronSchedule as required field

--- src/Chronos.Offboarding/OffboardingConfiguration.cs
+++ src/Chronos.Offboarding/OffboardingConfiguration.cs
@@ -3,6 +3,6 @@ namespace Chronos.Offboarding;
 public class OffboardingConfiguration
 {
     public const string Offboarding = nameof(Offboarding);
-    public string CronSchedule { get; set; }
+    public required string CronSchedule { get; set; }
     public int RetentionDays { get; set; }
 }
