7579626ab7d48c9642e71a93005ba525a5f0614b
ShalevKk <kayatsha@post.bgu.ac.il>
2026-04-14 11:17:29 +0300

    Offboarding configuration - modified retentionDays to be required

--- src/Chronos.Offboarding/OffboardingConfiguration.cs
+++ src/Chronos.Offboarding/OffboardingConfiguration.cs
@@ -4,5 +4,5 @@ public class OffboardingConfiguration
 {
     public const string Offboarding = nameof(Offboarding);
     public required string CronSchedule { get; set; }
-    public int RetentionDays { get; set; }
+    public required int RetentionDays { get; set; }
 }
